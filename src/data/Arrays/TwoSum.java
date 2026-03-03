c472363438dda166c1748675b0097b639a1db1ff
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-03-03 08:58:47 +0530

    coin change dp

--- src/data/Arrays/TwoSum.java
+++ src/data/Arrays/TwoSum.java
@@ -9,64 +9,61 @@ import java.util.Map;
 public class TwoSum {
     void main() {
 
-        int[] arr = {0, -1, 2, -3, 1};
-//    int[] arr={3, 2, 4};
+        int[] arr1 = {0, -1, 2, -3, 1};
+        int[] arr2 = {3, 2, 4};
         int target = -2;
-        System.out.println(Arrays.toString(twoSumHashMap(arr, target)));
+        System.out.println(Arrays.toString(twoSumTwoPointer(arr1, target)));
 
     }
 
 
     public static int[] twoSum(int[] arr, int target) {
         int[] result = new int[2];
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] + arr[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
+        for (int i = 0; i < arr.length; i++)
+            for (int j = i + 1; j < arr.length; j++)
+                if (arr[i] + arr[j] == target) return new int[]{i, j};
         return new int[]{-1, -1};
     }
 
     public static int[] twoSumTwoPointer(int[] arr, int target) {
-
         Arrays.sort(arr);
-
         int left = 0;
         int right = arr.length - 1;
-        System.out.println(Arrays.toString(arr));
-
         while (left < right) {
             int sum = arr[left] + arr[right];
-
-            if (sum == target) {
-                return new int[]{left, right};
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
-            }
+            if (sum == target)
+                return new int[]{arr[left], arr[right]};
+            else if (sum < target) left++;
+            else right--;
         }
         return new int[]{-1, -1};
     }
 
 
     public static int[] twoSumHashMap(int[] arr, int target) {
-
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
             int comp = target - arr[i];
-            if (map.containsKey(comp)) {
-                return new int[]{map.get(comp), i};
-            }
-
+            if (map.containsKey(comp)) return new int[]{map.get(comp), i};
             map.put(arr[i], i);
-
-
         }
         return new int[]{-1, -1};
-
     }
+
+
+    public static int[] two(int[] arr, int t) {
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int r = arr[left] + arr[right];
+            if (r == t) return new int[]{left, right};
+            else if (r < t) left++;
+            else right--;
+        }
+        return new int[]{-1, -1};
+    }
+
+
 }
---
+++ src/data/DynamicPrograming/CoinChange.java
@@ -0,0 +1,24 @@
+package data.DynamicPrograming;
+
+public class CoinChange {
+    /*
+📌 Problem
+
+You are given an array of coin denominations coins[] and an integer amount.
+
+Return the minimum number of coins required to make that amount.
+If it is not possible, return -1.
+
+     */
+    void main(){
+
+    }
+
+
+
+
+
+
+
+
+}
