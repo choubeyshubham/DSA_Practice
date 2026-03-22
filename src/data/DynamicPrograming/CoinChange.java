78613bad16b8ffd8d7393d8337e77029de0b1180
Shubham Choubey <shubhamchoubey96@gmail.com>
2026-03-22 11:58:53 +0530

    Coin Change

--- src/data/DynamicPrograming/CoinChange.java
+++ src/data/DynamicPrograming/CoinChange.java
@@ -11,7 +11,27 @@ If it is not possible, return -1.
 
      */
     void main(){
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        System.out.println(coin(coins, amount)); // Output: 3
 
+    }
+
+    public static int coin(int[] coins,int amount){
+        int[] dp=new int[amount+1];
+        for(int i=0;i<=amount;i++){
+            dp[i]=amount+1;
+        }
+        dp[0]=0;
+
+        for(int i=1;i<=amount;i++){
+            for(int coin:coins){
+                if(i-coin>=0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin]+1);
+                }
+            }
+        }
+        return dp[amount]>amount?-1:dp[amount];
     }
 
 
